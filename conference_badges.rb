7943d4da81d1cf08536a7258b520e255e5c04a34
Logan Smith <logsmith06@gmail.com>
2019-10-15 19:16:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -36,12 +36,12 @@ printed_badges = []
 
 attendees.each do |name|
   printed_rooms << assign_rooms(name)
-  
+
 end
 puts printed_rooms
 attendees.each do |name|
   printed_badges << batch_badge_creator(name)
-  
+
 end
 puts printed_badges
 end
